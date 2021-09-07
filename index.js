5d419124ecc08f41d45463cfbf0e509e336453a0
Lucas <lucasmbrute614@gmail.com>
2021-09-06 21:23:48 -0300

    index

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ app.use(express.json());
 Connect(db_url,db_user,db_pass,db_data);
 
 
-app.get("/filme", (req,res)=>{
-  res.send("<h1>Hello</h1>")
-})
+
 
 const Filmerota = require("./routes/filmes.routes");
 
