fba4fd9d790aef1bcd7e3c383b040f07fd6e5112
lucasmbrute2 <lucasmbrute614@gmail.com>
2021-09-09 13:24:56 -0300

    alterando cors

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config();
 
 }
+
 const express = require("express");
+const corsOptions = {
+  origin: 'http://localhost:3000/',
+  optionSucessStatus: 200 
+}
+const cors = require("cors");
 const app = express();
 const port = 3000;
 const Connect = require("./connect/connect");
@@ -11,6 +17,7 @@ const db_user = process.env.DB_USER
 const db_pass = process.env.DB_PASS
 const db_data = process.env.DB_DATA
 
+app.use(cors(corsOptions))
 app.use(express.json());
 Connect(db_url,db_user,db_pass,db_data);
 
